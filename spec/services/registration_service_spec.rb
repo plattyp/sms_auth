c235596ab1fc8892f5f657d86a00220a7d118de5
Andrew Platkin <andrew.platkin@gmail.com>
2017-01-22 22:29:36 -0600

    breaking up registration service and adding more specs

---
+++ spec/services/registration_service_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe RegistrationService do
+  describe '#register_user' do
+  end
+
+  describe '#send_verification_token' do
+  end
+
+  describe '#verify_user' do
+  end
+
+  describe '#create_authentication_token' do
+  end
+
+  describe '#verification_valid?' do
+  end
+
+  describe '#token_matches?' do
+  end
+
+  describe '#verification_error' do
+  end
+
+  describe '#append_login_attempt' do
+  end
+
+  describe '#create_and_verify_user' do
+  end
+
+  describe '#allowable_phone_number' do
+  end
+end
